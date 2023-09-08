0886138df1f6554e24084d59def2c61d34488c73
AriNga382557 <NACpra070448@gmail.com>
2023-09-08 23:52:28 +0700

    เปิดหน้า hint และวิธีการเล่น

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -16,3 +16,31 @@ function hidecode() {
     }
 }
 
+// เปิดหน้า hint
+function openCenterModal1() {
+  var centerModal1 = document.getElementById("centerModal1");
+  centerModal1.style.display = "block";
+}
+
+// ปิดหน้า hint
+function closeCenterModal1() {
+  var centerModal1 = document.getElementById("centerModal1");
+  centerModal1.style.display = "none";
+}
+
+// เปิดหน้า ?
+function openCenterModal2() {
+  var centerModal2 = document.getElementById("centerModal2");
+  centerModal2.style.display = "block";
+}
+
+// ปิดหน้า ?
+function closeCenterModal2() {
+  var centerModal2 = document.getElementById("centerModal2");
+  centerModal2.style.display = "none";
+}
+
+window.onload = function() {
+  closeCenterModal1();
+  closeCenterModal2();
+};
