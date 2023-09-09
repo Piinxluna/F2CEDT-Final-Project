9dedc6cec5a974aaa96ee4bba2fa0385740188b1
Piinxluna <pingpingkuss@gmail.com>
2023-09-09 16:18:14 +0700

    แก้ไฟล์

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -1,19 +1,19 @@
 function hidecode() {
-    var x = document.getElementById("mainCode");
-    const changeText = document.querySelector("#hideButton");
-    const changeMapSize = document.getElementById("mainPart");
-    const runCodeButton = document.getElementsByClassName("borderButton");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      changeText.textContent = "hide";
-      changeText.style.color = "#fbf7f8";
-      changeMapSize.style.width = "65%";
-    } else {
-      x.style.display = "none";
-      changeText.textContent = "show";
-      changeText.style.color = "#211217";
-      changeMapSize.style.width = "100%";
-    }
+  var x = document.getElementById("mainCode");
+  const changeText = document.querySelector("#hideButton");
+  const changeMapSize = document.getElementById("mainPart");
+  const runCodeButton = document.getElementsByClassName("borderButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    changeText.textContent = "hide";
+    changeText.style.color = "#fbf7f8";
+    changeMapSize.style.width = "65%";
+  } else {
+    x.style.display = "none";
+    changeText.textContent = "show";
+    changeText.style.color = "#211217";
+    changeMapSize.style.width = "100%";
+  }
 }
 
 // เปิดหน้า hint
@@ -40,9 +40,7 @@ function closeCenterModal2() {
   centerModal2.style.display = "none";
 }
 
-window.onload = function() {
+window.onload = function () {
   closeCenterModal1();
   closeCenterModal2();
 };
-
-
