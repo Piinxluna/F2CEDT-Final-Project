8f7e11a83512a033fdc15ecdccabf9ccd2c8dee6
Piinxluna <pingpingkuss@gmail.com>
2023-09-09 20:44:11 +0700

    callGetNewLevelAPI

--- frontend/public/scripts/api.js
+++ frontend/public/scripts/api.js
@@ -1,1 +1,13 @@
 // This is api
+import { BACKEND_URL } from "./config.js";
+
+/** @typedef {import("./config.js").Level} Level */
+
+export async function callGetNewLevelAPI(levelNumber) {
+  /** @type {Level[]} */
+  const newLevel = await fetch(
+    `${BACKEND_URL}/getNewLevel/${levelNumber}`
+  ).then((r) => r.json());
+
+  return newLevel;
+}
