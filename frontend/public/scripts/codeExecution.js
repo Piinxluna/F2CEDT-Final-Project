6e715a3498ea41395bd275c683055786f7f38ae0
piinxluna <pingpingkuss@gmail.com>
2023-09-21 10:29:19 +0700

    ทำ runcode function

--- frontend/public/scripts/codeExecution.js
+++ frontend/public/scripts/codeExecution.js
@@ -1,14 +1,46 @@
 import { callGetNewLevelAPI } from "./api.js";
-import { displayPos } from "./duckMove.js";
+import { displayPos, hideBabyDuck, isSamePoint } from "./duckMove.js";
 /** @typedef {import("./config.js").Level} Level */
 
-var exampleCodeLists = ["Walk", "Walk", "Turn left"];
 // For test only -> The final version should get arrays from codeInput field, then make it into array within runCode() and pass it to calcResult()
-var dir = 0; // 1 = Up, -1 = Down, 2 = Left, -2 = Right
+var exampleCodeLists = [
+  "Walk",
+  "Jump",
+  "Turn left",
+  "Walk",
+  "Jump",
+  "Walk",
+  "Turn left",
+  "Walk",
+  "Walk",
+  "Walk",
+];
+
+var duckPic = document.getElementById("mom-duck-pic");
+var blockSize;
+var dir = 0; // 1 = Up, 2 = Right, 3 = Down, 4 = Left,
 var goal = [0, 0];
+var startPos = [];
+var babyDuckPos = [];
+var mapArray = [];
+
 var pos = [0, 0];
+var nextPos = [];
 
 export async function runCode() {
+  duckPic.height = blockSize;
+  duckPic.width = blockSize;
+  pos = startPos;
+  await displayPos(
+    duckPic,
+    pos.map((x) => x * blockSize),
+    blockSize
+  );
+  for (let i = 0; i < 3; i++) {
+    let babyDuck = document.getElementById("baby-duck-pic-" + i);
+    babyDuck.style.display = "block";
+  }
+
   /* code here */
   var codeLists = exampleCodeLists;
 
@@ -18,72 +50,162 @@ export async function runCode() {
     // Show error message
   }
   if (result.isPass === true) {
-    // Show 'go to next level' ui
+    toFinalPage(result.babyCollected);
   }
 }
 
-export async function calcResult() {
+export async function calcResult(codeLists) {
   var babyCollected = 0;
   var errorWalk = 0;
   var isPass = false;
   var isError = false;
   var errorRes = null;
-  for (data in codeLists) {
-    if (pos[0] == goal[0] && pos[1] == goal[1]) {
+  for (let data of codeLists) {
+    isError = false;
+    if (data == "Walk") {
+      nextTarget(1);
+      if (getPosData(nextPos) == "-") {
+        pos = nextPos;
+        await displayPos(
+          duckPic,
+          pos.map((x) => x * blockSize),
+          blockSize
+        );
+      } else {
+        isError = true;
+      }
+    } else if (data == "Jump") {
+      nextTarget(1);
+      if (getPosData(nextPos) == "-") {
+        pos = nextPos;
+        await displayPos(
+          duckPic,
+          pos.map((x) => x * blockSize),
+          blockSize
+        );
+      } else if (getPosData(nextPos) == "r") {
+        nextTarget(2);
+        pos = nextPos;
+        await displayPos(
+          duckPic,
+          pos.map((x) => x * blockSize),
+          blockSize
+        );
+      } else {
+        isError = true;
+      }
+    } else if (data == "Turn left") {
+      dir--;
+      dir = dir == 0 ? 4 : dir;
+    } else if (data == "Turn right") {
+      dir++;
+      dir = dir == 5 ? 1 : dir;
+    } else if (data.slice(0, 3) == "For") {
+    }
+
+    for (let i in babyDuckPos) {
+      if (isSamePoint(pos, babyDuckPos[i])) {
+        babyCollected++;
+        hideBabyDuck(babyDuckPos[i].map((x) => x * blockSize));
+        babyDuckPos.splice(i, 1);
+      }
+    }
+
+    // If can't walk -> errorWalk++ -> reset every time after can walk
+    if (isError == true) {
+      errorWalk++;
+    } else {
+      errorWalk = 0;
+    }
+
+    console.log(pos, goal);
+    if (isSamePoint(pos, goal)) {
       // Walk to finish point
       isPass = true;
       break;
     } else if (errorWalk > 3) {
-      errorRes = "Error walk";
+      errorRes = "เดินไปทางนั้นไม่ได้อ่ะ T^T";
       break;
     }
-
-    if (data == "Walk") {
-    } else if (data == "Jump") {
-    } else if (data == "Turn left") {
-    } else if (data == "Turn right") {
-    } else if (data.slice(0, 3) == "For") {
-    }
-
-    // Check baby -> babyCollected++
-    // If can't walk -> errorWalk++ -> reset every time after can walk
   }
   return { errorRes, isPass, babyCollected };
+
+  // ---------- functions used to calcresult ----------
+  function nextTarget(i) {
+    if (dir == 1) {
+      nextPos = [pos[0] - i, pos[1]];
+    } else if (dir == 2) {
+      nextPos = [pos[0], pos[1] + i];
+    } else if (dir == 3) {
+      nextPos = [pos[0] + i, pos[1]];
+    } else if (dir == 4) {
+      nextPos = [pos[0], pos[1] - i];
+    }
+  }
+
+  function getPosData(pos) {
+    if (
+      pos[0] < 0 ||
+      pos[1] < 0 ||
+      pos[0] >= mapArray.length ||
+      pos[1] >= mapArray.length
+    ) {
+      return null;
+    } else {
+      return mapArray[pos[0]][pos[1]];
+    }
+  }
 }
 
 export async function showNewLevel(levelNumber) {
-
   let newLev = await callGetNewLevelAPI(levelNumber);
 
   //change level number
-  document.getElementById("levelNum").textContent = `Level ${newLev.levelNumber}`;
+  document.getElementById(
+    "levelNum"
+  ).textContent = `Level ${newLev.levelNumber}`;
 
   //change mapfile
-  document.getElementById("mapLevel").src = `${newLev.mapfile}`;
-
-  //change map array
-  //change mom duck position
-  //change baby duck position
-  //change goal position
-
-  //change code guide
-  document.getElementById("helpContent").textContent = `${newLev.codeGuide}`;
+  document.getElementById("map-background").src = `${newLev.mapFile}`;
 
   //change hint
   document.getElementById("hintContent").textContent = `${newLev.hint}`;
 
+  //change map array
+  mapArray = newLev.mapArray;
+
   var mapSize = document.getElementById("map-background").clientWidth;
-  var blockSize = mapSize / newLev.mapArray.length;
-  var duckPic = document.getElementById("mom-duck-pic");
+  blockSize = mapSize / newLev.mapArray.length;
+
+  //change mom duck position
   duckPic.height = blockSize;
   duckPic.width = blockSize;
-  pos[0] = newLev.momDuckStartPos[0] * blockSize;
-  pos[1] = newLev.momDuckStartPos[1] * blockSize;
-  displayPos(duckPic, pos, blockSize);
+  startPos = newLev.momDuckStartPos;
+  pos = startPos;
+  displayPos(
+    duckPic,
+    pos.map((x) => x * blockSize),
+    blockSize
+  );
 
+  //change goal position
   dir = newLev.momDuckStartDir;
-  goal[0] = newLev.goal[0];
-  goal[1] = newLev.goal[1];
+  goal[0] = newLev.goalPos[0];
+  goal[1] = newLev.goalPos[1];
+
+  //change baby duck position
+  for (let i = 0; i < newLev.babyDuckPos.length; i++) {
+    let babyDuck = document.getElementById("baby-duck-pic-" + i);
+    babyDuck.height = blockSize;
+    babyDuck.width = blockSize;
+    displayPos(
+      babyDuck,
+      newLev.babyDuckPos[i].map((x) => x * blockSize),
+      blockSize
+    );
+    babyDuckPos[i] = newLev.babyDuckPos[i];
+  }
+
   // Call function show code input
 }
 
@@ -117,7 +239,7 @@ export async function toFinalPage(levelScore) {
   const winPage = document.getElementById("win");
   const losePage = document.getElementById("lose");
   // let thisLevel = callGetNewLevelAPI(levelNumber);
-  if(levelScore===0) {
+  if (levelScore === 0) {
     winPage.style.display = "none";
     losePage.style.display = "block";
   } else {
--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -4,20 +4,12 @@ showNewLevel(0);
 
 document.addEventListener("DOMContentLoaded", () => {
   /** @type {HTMLButtonElement} */
-  const testButton = document.getElementById("runCodeButton");
-  testButton.addEventListener("click", () => {
-    let levelNum = document.getElementById("levelNum").innerText;
-    levelNum = levelNum.slice(6);
-    showNewLevel(levelNum);
-  });
-
   const runCodeButton = document.getElementById("runCodeButton");
   runCodeButton.addEventListener("click", () => {
     runCode();
   });
   const addInputButton = document.getElementById("add-input-button");
   addInputButton.addEventListener("click", () => {
-    console.log("Hi");
     addInputLine();
   });
 });
