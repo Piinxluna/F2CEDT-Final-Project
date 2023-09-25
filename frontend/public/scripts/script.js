9c916731852e76bdfa46b986d96de75c6dabc72c
pppny1920 <popo192029@gmail.com>
2023-09-25 20:52:33 +0700

    add runCodeButton2

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -2,19 +2,18 @@ function hidecode() {
 	var x = document.getElementById('mainCode')
 	const changeText = document.querySelector('#hideButton')
 	const changeMapSize = document.getElementById('mainPart')
-	let runCode = document.getElementById('runCodeButton')
 	if (x.style.display === 'none') {
 		x.style.display = 'block'
 		changeText.textContent = 'hide'
 		changeText.style.color = '#fbf7f8'
 		changeMapSize.style.width = '65%'
-		runCode.style.bottom = '5%'
+		document.getElementById('runCodeButton').style.display = 'none'
 	} else {
 		x.style.display = 'none'
 		changeText.textContent = 'show'
 		changeText.style.color = '#211217'
 		changeMapSize.style.width = '100%'
-		runCode.style.bottom = '11%'
+		document.getElementById('runCodeButton').style.display = 'block'
 	}
 }
 
