e4719025a1f7d7dfa2a8dde4022fce140efdba39
pppny1920 <popo192029@gmail.com>
2023-09-25 20:31:26 +0700

    edit IDname in hidecode()

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -2,30 +2,19 @@ function hidecode() {
 	var x = document.getElementById('mainCode')
 	const changeText = document.querySelector('#hideButton')
 	const changeMapSize = document.getElementById('mainPart')
-	var mapCenter = document.getElementsByClassName('centerimg')
-	var runCode = document.getElementsByClassName('borderButton')
+	let runCode = document.getElementById('runCodeButton')
 	if (x.style.display === 'none') {
 		x.style.display = 'block'
 		changeText.textContent = 'hide'
 		changeText.style.color = '#fbf7f8'
 		changeMapSize.style.width = '65%'
-		for (let i = 0; i < mapCenter.length; ++i) {
-			mapCenter[i].style.width = '50%'
-		}
-		for (let i = 0; i < runCode.length; ++i) {
-			runCode[i].style.bottom = '5%'
-		}
+		runCode.style.bottom = '5%'
 	} else {
 		x.style.display = 'none'
 		changeText.textContent = 'show'
 		changeText.style.color = '#211217'
 		changeMapSize.style.width = '100%'
-		for (let i = 0; i < mapCenter.length; ++i) {
-			mapCenter[i].style.width = '40%'
-		}
-		for (let i = 0; i < runCode.length; ++i) {
-			runCode[i].style.bottom = '11%'
-		}
+		runCode.style.bottom = '11%'
 	}
 }
 
