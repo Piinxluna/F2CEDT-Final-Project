59308a6e69ddb53df5a10f5e2ed576d58c0fe873
piinxluna <pingpingkuss@gmail.com>
2023-09-26 22:22:55 +0700

    แก้ปุ่ม add แล้วเพิ่ม 12 บรรทัด

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -13,32 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	runCodeButton.addEventListener('click', () => {
 		runCode()
 	})
-	const addInputButton = document.getElementById('add-input-button')
-	addInputButton.addEventListener('click', () => {
-		addInputLine()
-	})
-	const delInputButton = document.getElementById('delete-input-button')
-	delInputButton.addEventListener('click', () => {
-		deleteInputLine()
-	})
-
-	const playAgainButton1 = document.getElementById('play-again-button-win')
-	playAgainButton1.addEventListener('click', showThisLevel)
-	const playAgainButton2 = document.getElementById('play-again-button-lose')
-	playAgainButton2.addEventListener('click', showThisLevel)
-
-	const nextLevelButton = document.getElementById('next-level-button')
-	nextLevelButton.addEventListener('click', () => {
-		let levelNum = document.getElementById('levelNum').innerText
-		levelNum = levelNum.slice(6)
-		const finPage = document.getElementById('gameOver')
-		finPage.style.display = 'none'
-		showNewLevel(levelNum)
-	})
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-	/** @type {HTMLButtonElement} */
 	const runCodeButton2 = document.getElementById('runCodeButton2')
 	runCodeButton2.addEventListener('click', () => {
 		runCode()
@@ -66,7 +40,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		showNewLevel(levelNum)
 	})
 })
-
 
 function showThisLevel() {
 	let levelNum = document.getElementById('levelNum').innerText
