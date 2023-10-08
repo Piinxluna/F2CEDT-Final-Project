b5a7d10ceb82ed593ce354f45b8bde228f886a61
Wuaimi <10327sornpicha@gmail.com>
2023-10-08 08:30:37 +0700

    delete in for เสร้จเเล้วคับ

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -35,27 +35,27 @@ function checkFor(object, codeGuide) {
 			object.id.slice(0, 9) + 'div-' + forId
 		)
 		let forDetail = document.createElement('div')
-		forDetail.innerHTML = `<div>
-      <label class="for-informm">int i = 0; i < </label>
-      <input type="text" class="for-i" id="condition-value-${forId}">
+		forDetail.innerHTML = `<div class="in-for">
+      <label class="for-informm">int i = 0; i </label>
+      <label><input type="text" class="for-i" id="condition-value-${forId}"></label>
       <label class="for-informm">; i++</label>
 		</div>`
-		forDetail.innerHTML += `<div>
+		forDetail.innerHTML += `<div id="for-code-input">
 		<p> { </p>
 		<div id="for-code-input-${forId}">
 			<div id="for-${forId}-movement-div-1">
       	<label for="for-${forId}-movement-1" class="order-dropdown"> 1 : </label>
-      	<select id="for-${forId}-movement-1" class="dropdown-select">
+      	<label><select id="for-${forId}-movement-1" class="dropdown-select">
         	<option disabled>-Choose Option-</option>
         	<option value="walk" class="dropdown-choice">walk()</option>
         	<option value="jump" class="dropdown-choice">jump()</option>
         	<option value="turn left" class="dropdown-choice">turn left()</option>
        		<option value="turn right" class="dropdown-choice">turn right()</option>
-      	</select>
+      	</select></label>
     	</div>
 		</div>
 		<button id="add-for-input-button-${forId}" class="fillButton add-del-button" onclick="addForInput(this,${exportedCodeGuide})">add</button>
-    <button id="delete-for-input-button-${forId}" class="fillButton add-del-button">delete</button>
+    <button id="delete-for-input-button-${forId}" class="fillButton add-del-button" onclick="deleteForInput(this)">delete</button>
 		<p> } </p>
 		</div>`
 		forDetail.setAttribute('id', `for-detail-div-${forId}`)
@@ -84,5 +84,17 @@ function addForInput(object, codeGuide) {
 		newdropdown.innerHTML = optionHTML
 		newdropdown.setAttribute('id', `for-${forId}-movement-div-${inputInd}`)
 		codeInput.appendChild(newdropdown)
+	}
+}
+
+function deleteForInput(object) {
+	let forId = object.id.slice(24)
+	const forNoInnerId = document.getElementById(`for-code-input-${forId}`)
+	let numFor = forNoInnerId.children.length
+	if (numFor > 1) {
+		let lastForLine = document.getElementById(
+			`for-${forId}-movement-div-${numFor}`
+		)
+		lastForLine.remove()
 	}
 }
